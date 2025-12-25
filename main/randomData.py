18c1e81553a90f05ac5e018e2b47e4ea03f5207e
EvgeniyKazancev <ekazancev777@gmail.com>
2025-12-25 22:59:34 +0300

    Сохранение данных в csv файл

--- main/randomData.py
+++ main/randomData.py
@@ -2,7 +2,12 @@ import pandas as pd
 import numpy as np
 
 def save_series():
+    """Функция генерации случайных  данных и создание обьекта Series.
+     Возвращает: обьект Series с 1000 случайных целых чисел"""
     series = pd.Series(np.random.randint(-10000, 10000, 1000))
+
+    # Сохранение данных в csv файл для дальнейшего использования
+    #series.to_csv('generate_series.csv', header=['Значения'])
     return series
 
 
